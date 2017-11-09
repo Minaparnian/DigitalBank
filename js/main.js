610205816a8beb2b11ac01d36c70d4feeda90156
Mina Parnian <minaparnian66@gmail.com>
2017-11-09 16:35:51 +1100

    show the offers name

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+const showNames = function (data) {
+  // console.log( data );
+
+
+  _(data.comingSoon).each(function (p) {
+    console.log( p );
+    const html = `<p>${ p.name } raised $${ p.raisedAmount }</p>`;
+
+    $('body').append( html );
+  })
+};
+
+//
+// $.ajax("https://api.equitise.exchange/offer").done( function (data) {
+// console.log( data );
+//
+// for (let i = 0; i < data.comingSoon.length; i++) {
+//   let item = data.comingSoon[i].name;
+// }
+//
+// _(data.comingSoon.name).each(function (p) {
+//   console.log( p );
+// })
+// });
+
+const xinjaURL = "https://api.equitise.exchange/offer";
+
+$.ajax(xinjaURL, {
+  format: 'json'
+}).done(showNames);
+
+
+
+
+
 $(document).ready(function(){
 // add class and remove the class to have animated without refreshing the page
   $('.left').mouseover(function(e) {
@@ -14,16 +49,5 @@ $(document).ready(function(){
     $("h1").addClass('animated slideInLeft');
 
   });
-
-  $.ajax("https://api.equitise.exchange/offer").done( function (data) {
-  console.log( data );
-  for (let i = 0; i < data.comingSoon.length; i++) {
-    let item = data.comingSoon[i];
-
-}
-
-
-});
-
 
 });
